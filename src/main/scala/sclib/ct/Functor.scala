c006038bf1454b3c363c985aae30b01c95ef95a9
j-keck <jhyphenkeck@gmail.com>
2016-04-09 15:14:21 +0200

    fix a silly mistake
    `flatMap` belongs to `Monad`

--- src/main/scala/sclib/ct/Functor.scala
+++ src/main/scala/sclib/ct/Functor.scala
@@ -5,5 +5,4 @@ package sclib.ct
   */
 trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 }
--- src/main/scala/sclib/ct/Monad.scala
+++ src/main/scala/sclib/ct/Monad.scala
@@ -8,4 +8,5 @@ trait Monad[F[_]] extends Functor[F]{
   /** lift the given value in the `Monad` */
   def pure[A](a: A): F[A]
 
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 }
