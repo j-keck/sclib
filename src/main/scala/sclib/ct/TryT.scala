81deaaf9c303ca728284f5624c5190392e539ce7
j-keck <jhyphenkeck@gmail.com>
2016-04-17 14:54:13 +0200

    'Try' monad transformer

---
+++ src/main/scala/sclib/ct/TryT.scala
@@ -0,0 +1,38 @@
+package sclib.ct
+
+import scala.util.{Failure, Success, Try}
+import sclib.ops.`try`._
+
+/**
+  * minimalistic `Try` monad transformer
+  *
+  * @example
+  * {{{
+  * scala> import sclib.ct._
+  * scala> import sclib.ops.`try`._
+  * scala> val tt = TryT[({type L[A] = Function1[Int, A]})#L, Int]{i => if(i < 10) i.success else "BOOM".failure}
+  *
+  * scala> tt.map(_ * 10).runTryT(5)
+  * res0: scala.util.Try[Int] = Success(50)
+  *
+  * scala> tt.map(_ * 10).runTryT(10)
+  * res1: scala.util.Try[Int] = Failure(java.lang.Exception: BOOM)
+  * }}}
+  *
+  */
+case class TryT[F[_], A](runTryT: F[Try[A]]) {
+
+  def map[B](f: A => B)(implicit F: Functor[F]): TryT[F, B] = TryT {
+    F.map(runTryT)(_.map(f))
+  }
+
+
+  def flatMap[B](f: A => TryT[F, B])(implicit F: Monad[F]): TryT[F, B] = TryT {
+    F.flatMap(runTryT)(_.fold(t => F.pure(t.failure[B]))(f(_).runTryT))
+  }
+
+
+  def flatMapF[B](f: A => F[Try[B]])(implicit F: Monad[F]): TryT[F, B] = TryT {
+    F.flatMap(runTryT)(_.fold(t => F.pure(t.failure[B]))(x => f(x)))
+  }
+}
