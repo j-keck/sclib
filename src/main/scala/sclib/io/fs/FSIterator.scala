76818f0140b6032832fd32b120a17a800e7ac648
j-keck <jhyphenkeck@gmail.com>
2016-05-03 09:53:05 +0200

    FSIterator: "don't look ahead"
     - the step's in the 'next()' function, from 'FSIterator' was:
       * check if the current element is a dir
       * if it's a dir, look inside and save the content's in 'todo'
       * return the current element to the caller
     - if the caller need to fix the dir permission - the "look ahead"
       fails.
     - fix: delay the 'look into dir' after the caller has processed
       the current element

--- src/main/scala/sclib/io/fs/FSIterator.scala
+++ src/main/scala/sclib/io/fs/FSIterator.scala
@@ -13,38 +13,50 @@ import sclib.ops.all._
   * it wraps every file operation in a `Try`.
   *
   * @example
-  *
   * {{{
-  * scala>  import sclib.io._
-  * scala> val xs = for {
+  * scala> import sclib.io._
+  * scala> import sclib.ops.`try`._
+  * scala> for {
   *      |   wd <- dir("sclib.io")
   *      |   wd <- wd.createTemp
   *      |   pub <- dir(wd, "pub").flatMap(_.create)
   *      |   _ <- file(pub, "a-file").flatMap(_.create)
   *      |   sec <- dir(wd, "sec").flatMap(_.create(0))
-  *      |   res = FSIterator(wd).toList
+  *      |   names = FSIterator(wd).map(_.fold(_.getClass.getName)(_.name)).toList.sorted
   *      |   _ <- sec.chmod(700)
   *      |   _ <- wd.deleteR
-  *      | } yield res
-  * xs: scala.util.Try[List[scala.util.Try[sclib.io.FSEntry.FSEntryImpl]]] = Success(List(Success(FSDir(/tmp/sclib.io7738690949969632606/sec)), Failure(java.nio.file.AccessDeniedException: /tmp/sclib.io7738690949969632606/sec), Success(FSDir(/tmp/sclib.io7738690949969632606/pub)), Success(FSFile(/tmp/sclib.io7738690949969632606/pub/a-file))))
+  *      | } yield names
+  * res0: scala.util.Try[List[String]] = Success(List(a-file, java.nio.file.AccessDeniedException, pub, sec))
   * }}}
   * @param start directory to start
   * @param maxDepth the maximum number of directory levels to visit
   */
 case class FSIterator(start: FSDir, maxDepth: Int = Integer.MAX_VALUE) extends Iterator[Try[FSEntryImpl]] {
 
-  private var todo: List[Try[FSEntryImpl]] = ls(start.path)
+  private var todo: List[Try[FSEntryImpl]]   = ls(start.path)
+  private var maybePendingDir: Option[FSDir] = None
 
-  override def hasNext: Boolean = todo.nonEmpty
+  override def hasNext: Boolean = {
+    // if we have a dir pending, push the content at the top (deep first) of the todo list
+    maybePendingDir.foreach { last =>
+      todo = ls(last.path) ++ todo
+      maybePendingDir = None
+    }
+
+    todo.nonEmpty
+  }
 
   override def next(): Try[FSEntryImpl] = {
     val curOrErr: Try[FSEntryImpl] = todo.head
     todo = todo.tail
 
-    // if the current entry is a directory, push the content at the top (deep first)
     curOrErr.foreach { cur =>
-      if (cur.isDirectory && cur.depth(start) < maxDepth) {
-        todo = ls(cur.path) ++ todo
+      // if the current entry is a directory, and we didn't reach 'maxDepth',
+      // save it to process the contents later.
+      // "don't look ahead" - necessary for example if we need to fix the permissions before we can look in the dir.
+      cur match {
+        case d: FSDir if d.depth(start) < maxDepth => maybePendingDir = Some(d)
+        case _                                     => // nothing to do
       }
     }
 
