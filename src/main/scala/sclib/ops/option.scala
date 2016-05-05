af8fab63d07f6ff214bab76d44cb796f07125222
j-keck <jhyphenkeck@gmail.com>
2016-05-05 08:36:06 +0200

    add 'Traversable[Option[A]].sequence'
     * reducing many `Option`s into a single `Option`

--- src/main/scala/sclib/ops/option.scala
+++ src/main/scala/sclib/ops/option.scala
@@ -1,5 +1,7 @@
 package sclib.ops
 
+import scala.collection.generic.CanBuildFrom
+
 object option extends option
 
 /**
@@ -22,4 +24,43 @@ trait option {
     def some: Option[A] = Some(a)
   }
 
+
+  /**
+    * reducing many `Option`s into a single `Option`
+    */
+  implicit class TraversableOfOption[A, CC[X] <: Traversable[X]](ts: CC[Option[A]]) {
+    /**
+      * reducing many `Option`s into a single `Option`
+      *
+      * @example {{{
+      * scala> import sclib.ops.option._
+      * scala> List(3.some, 44.some).sequence
+      * res0: Option[List[Int]] = Some(List(3, 44))
+      *
+      * scala> List(3.some, none, 44.some).sequence
+      * res1: Option[List[Int]] = None
+      *
+      * scala> Vector(1.some, 2.some).sequence
+      * res2: Option[scala.collection.immutable.Vector[Int]] = Some(Vector(1, 2))
+      * }}}
+      */
+    def sequence(implicit cbf: CanBuildFrom[Nothing, A, CC[A]]): Option[CC[A]] = {
+
+      val b = {
+        val builder = collection.breakOut[CC[Option[A]], A, CC[A]]
+        builder(ts)
+      }
+      b.sizeHint(ts)
+
+      def go(xs: Traversable[Option[A]]): Option[CC[A]] = xs.headOption match {
+        case Some(None) => None
+        case Some(Some(x)) =>
+          b += x
+          go(xs.tail)
+        case None => b.result.some
+      }
+
+      go(ts)
+    }
+  }
 }
